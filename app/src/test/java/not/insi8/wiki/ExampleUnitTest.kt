6b04bd081fbde9a915b2555bfe4230fd98ead16d
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-09-29 23:41:48 +0200

    updating packages.

--- app/src/test/java/not/insi8/wiki/ExampleUnitTest.kt
+++ app/src/test/java/not/insi8/wiki/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.vipps.wiki
+package not.insi8.wiki
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
